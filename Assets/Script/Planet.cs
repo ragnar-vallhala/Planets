ce24c1cf28f369c652ec5309903e2ab0aeeaee77
ashutosh <ashut@TIGER>
2023-04-23 03:09:55 +0530

    One more

--- Assets/Script/Planet.cs
+++ Assets/Script/Planet.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    [SerializeField] private float mass = 100.0f;
+    [SerializeField] private float mass = 100000000.0f;
     [SerializeField] private float k = 100.0f;
 
 
@@ -16,10 +16,14 @@ public class Planet : MonoBehaviour
         foreach (Planet body in bodies)
         {
             Vector3 r = body.transform.position - transform.position;
-            r.Normalize();
-            Vector3 accel = (k * body.mass / (r.magnitude * r.magnitude))*r;
+            
+            float accel = (k * body.mass / (r.magnitude * r.magnitude));
 
-            Vector3 velocity = accel * Time.deltaTime;
+            r.Normalize();
+
+            Vector3 acc = accel * r;
+
+            Vector3 velocity = acc * Time.deltaTime;
             transform.position += velocity * Time.deltaTime;
 
             
